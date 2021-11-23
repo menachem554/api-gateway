aa7457f42b38581e20292d03723060b99a729b96
menachem554 <mendyrivkin@gmail.com>
2021-11-23 15:06:19 +0200

    m

--- bookstore/controller.go
+++ bookstore/controller.go
@@ -7,8 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
-	"github.com/menachem554/api-geteway/bookstoregit status
-	/entity.go"
+	"github.com/menachem554/api-geteway/bookstore/entity.go"
 	pb "github.com/menachem554/Bookstore/proto"
 )
 
